b9a057cada0b9421f523dfd2bd9a02239b6ec41b
daniel09.09.01 <daniel09.09.01@gmail.com>
2020-08-20 14:55:53 +0300

    add comment model

---
+++ app/src/main/java/com/example/myapplication/models/Comment.java
@@ -0,0 +1,22 @@
+package com.example.myapplication.models;
+
+public class Comment {
+    String Username;
+    String Comment;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getComment() {
+        return Comment;
+    }
+
+    public void setComment(String comment) {
+        Comment = comment;
+    }
+}
